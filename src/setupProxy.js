51aee9747f95fceaed4630db55ab161a13660307
hitesh9929 <hiteshabishnoi@gmail.com>
2023-10-15 01:08:17 +0530

    one last time

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://c2cf-2401-4900-6196-e6b2-d499-a37f-b5ce-ef0f.ngrok-free.app ',
+      target: 'https://kannada-koota-tickets.vercel.app',
       changeOrigin: true,
     })
   );
